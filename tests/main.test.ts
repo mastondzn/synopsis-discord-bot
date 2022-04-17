d0732e4bd1a7992c4dd8e0b473ceda9863b1f4c6
Maston <MastonDZN@users.noreply.github.com>
2022-04-17 21:33:16 +0300

    remove tests from template

--- tests/main.test.ts
+++
@@ -1,9 +0,0 @@
-import { add, subtract } from '../src/main';
-
-test('add 2+4 to equal 6', () => {
-    expect(add(2, 4)).toBe(6);
-});
-
-test('subtract 4-2 to equal 2', () => {
-    expect(subtract(4, 2)).toBe(2);
-});
