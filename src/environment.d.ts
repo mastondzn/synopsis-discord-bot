a121175231ade6ab1bfad85ef3d6c5bf8f437499
Maston <MastonDZN@users.noreply.github.com>
2022-04-21 02:40:44 +0300

    update process.env types

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,7 +1,11 @@
 declare namespace NodeJS {
     // eslint-disable-next-line unicorn/prevent-abbreviations
     interface ProcessEnv {
-        readonly NODE_ENV?: 'development' | 'production';
-        readonly EXAMPLE_VAR?: 'EXAMPLE_VALUE';
+        readonly NODE_ENV: 'development' | 'production';
+        readonly MONGODB_URL: string;
+        readonly MONGODB_DB_NAME: string;
+        readonly REDIS_PWD: string;
+        readonly REDIS_HOST: string;
+        readonly REDIS_PORT: string;
     }
 }
