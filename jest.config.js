f90569ae3164fbe43b121234025ac7956c931b71
Maston <MastonDZN@users.noreply.github.com>
2022-05-18 21:58:03 +0300

    update jest config to use ts comment

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
-module.exports = {
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     transform: {
         '^.+\\.ts$': 'ts-jest',
     },
-    testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.ts$',
+    testRegex: '((?<!build)/tests/.*|(.|/)(test|spec)).ts$',
     coverageDirectory: 'coverage',
     collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+    passWithNoTests: true, // TODO: xd
 };
+
+module.exports = config;
