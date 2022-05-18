667c262d49d8a2ce818f41cf844a6188b24d6fde
Maston <MastonDZN@users.noreply.github.com>
2022-05-18 22:04:47 +0300

    remove test user db entity

--- src/db/entitites/test-user.ts
+++
@@ -1,9 +0,0 @@
-import { Entity, Property } from '@mikro-orm/core';
-
-import { BaseEntity } from './base-entity';
-
-@Entity()
-export class TestUser extends BaseEntity {
-    @Property()
-    name!: string;
-}
