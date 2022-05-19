e10bf4bea77bacc4f3df87bf0501ecdd49513a74
Maston <MastonDZN@users.noreply.github.com>
2022-05-19 21:40:27 +0300

    delete unused types

--- src/utils/types/mikro.ts
+++
@@ -1,6 +0,0 @@
-import type { Configuration, Options } from '@mikro-orm/core';
-import type { MongoDriver } from '@mikro-orm/mongodb';
-
-export type MikroWithMongoDriverOptions =
-    | Options<MongoDriver>
-    | Configuration<MongoDriver>;
